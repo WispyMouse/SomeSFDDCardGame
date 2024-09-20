42bf4a74f0beffebaded4fae66226e70a007c634
Mint Gould <wispymouse@gmail.com>
2024-09-19 21:47:41 -0700

    9/19 UX Improvements
    Panel Popup
    Moving Around UX

--- Unity/Assets/Scripts/UX/MouseHoverShowerPanel.cs
+++ Unity/Assets/Scripts/UX/MouseHoverShowerPanel.cs
@@ -15,6 +15,17 @@ namespace SFDDCards.UX
         [SerializeReference]
         public RectTransform OwnTransform;
 
+        [SerializeReference]
+        private PopupPanel PopupPanelPF;
+
+        [SerializeReference]
+        private Transform RightPopupPanelHolderTransform;
+
+        [SerializeReference]
+        private Transform LeftPopupPanelHolderTransform;
+
+        private List<PopupPanel> ActivePanels { get; set; } = new List<PopupPanel>();
+
         public void SetFromHoverListener(IMouseHoverListener listener)
         {
             if (listener.TryGetCard(out Card toShow))
@@ -36,12 +47,78 @@ namespace SFDDCards.UX
         {
             this.OverlayCard.gameObject.SetActive(true);
             this.OverlayCard.SetFromCard(toShow);
+            this.SetPopupPanelsFromCard(toShow);
         }
 
         private void HideCard()
         {
             this.OverlayCard.Annihilate();
             this.OverlayCard.gameObject.SetActive(false);
+        }
+        private void SetPopupPanelsFromCard(Card toShow)
+        {
+            List<string> texts = new List<string>();
+
+            texts.Add($"<b>Tags:</b> {string.Join(", ", toShow.Tags)}");
+
+            this.SetPopupPanels(texts);
+        }
+
+        private void SetPopupPanels(List<string> text)
+        {
+            for (int ii = this.RightPopupPanelHolderTransform.childCount - 1; ii >= 0; ii--)
+            {
+                Destroy(this.RightPopupPanelHolderTransform.GetChild(ii).gameObject);
+            }
+
+            for (int ii = this.LeftPopupPanelHolderTransform.childCount - 1; ii >= 0; ii--)
+            {
+                Destroy(this.LeftPopupPanelHolderTransform.GetChild(ii).gameObject);
+            }
+
+            for (int ii = this.ActivePanels.Count - 1; ii >= 0; ii--)
+            {
+                if (this.ActivePanels[ii] != null)
+                { 
+                    Destroy(this.ActivePanels[ii].gameObject);
+                }
+            }
+
+            this.ActivePanels.Clear();
+            Transform holder = this.RightPopupPanelHolderTransform;
+            bool usingLeftHolder = false;
+
+            if (this.transform.position.x >= Screen.width / 2f)
+            {
+                holder = this.LeftPopupPanelHolderTransform;
+                usingLeftHolder = true;
+            }
+
+            foreach (string currentText in text)
+            {
+                PopupPanel panel = Instantiate(this.PopupPanelPF, holder);
+                panel.SetText(currentText);
+                this.ActivePanels.Add(panel);
+
+                if (usingLeftHolder)
+                {
+                    panel.LayoutGroup.childAlignment = TextAnchor.UpperRight;
+                }
+            }
+        }
+
+        public List<RectTransform> GetAllRectTransforms()
+        {
+            List<RectTransform> transforms = new List<RectTransform>();
+
+            transforms.Add(this.OverlayCard.GetComponent<RectTransform>());
+
+            foreach (PopupPanel panel in this.ActivePanels)
+            {
+                transforms.Add(panel.GetComponent<RectTransform>());
+            }
+
+            return transforms;
         }
     }
 }
